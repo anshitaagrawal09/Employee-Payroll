9a6ae84f32075790add90b961b875b732763b35f
anshitaagrawal09 <anshitaagrawal75@gmail.com>
2025-03-06 11:15:02 +0530

    Introducing DTO and Model

--- src/main/java/com/example/EmployeePayroll/model/Employee.java
+++ src/main/java/com/example/EmployeePayroll/model/Employee.java
@@ -1,11 +1,8 @@
 package com.example.EmployeePayroll.model;
 
+//UC3
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "employees")
@@ -18,9 +15,38 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name is mandatory")
     private String name;
-
-    @Min(value = 1000, message = "Salary must be at least 1000")
     private double salary;
+
+    public Employee(EmployeeDTO employeeDTO) {
+        this.name = employeeDTO.getName();
+        this.salary = employeeDTO.getSalary();
+    }
 }
+
+
+//UC2
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.Min;
+//import jakarta.validation.constraints.NotBlank;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table(name = "employees")
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Employee {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @NotBlank(message = "Name is mandatory")
+//    private String name;
+//
+//    @Min(value = 1000, message = "Salary must be at least 1000")
+//    private double salary;
+//}
